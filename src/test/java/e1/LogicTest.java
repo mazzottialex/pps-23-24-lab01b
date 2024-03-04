02d6314ad27b8693bbbe9a15b503e6a2acd84dce
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-03-04 11:32:32 +0100

    check if is present knight

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,21 +1,36 @@
 package e1;
 import org.junit.jupiter.api.*;
 
-import e2.Logics;
-import e2.LogicsImpl;
+import e1.Logics;
+import e1.LogicsImpl;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class LogicTest {
 
-  private Logics log;
+  private Logics log = new LogicsImpl(5);
+  private int size = 5;
 
   @BeforeEach
   void init(){
-    log = new LogicsImpl(5);
+    log = new LogicsImpl(size);
+  }
+
+  @Test 
+  void checkGenerateKnight(){
+    Boolean checkKnight = false;
+    for(int i = 0; i<size; i++){
+      for(int j = 0;j<size; j++ ){
+        if(log.hasKnight(i, j)){
+          checkKnight = true;
+        }
+      }
+    }
+    assertTrue(checkKnight);
   }
 
   @Test
   public void test() {
+    
     assertTrue(true);
     // TODO: Add your test logic here
     // You can generate random inputs and assert the expected output
