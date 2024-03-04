0a533934dfaaa93e18ec9de64ebce2bc57e7e9c6
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-03-04 11:07:10 +0100

    init logics

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,11 +1,22 @@
 package e1;
 import org.junit.jupiter.api.*;
+
+import e2.Logics;
+import e2.LogicsImpl;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class LogicTest {
 
+  private Logics log;
+
+  @BeforeEach
+  void init(){
+    log = new LogicsImpl(5);
+  }
+
   @Test
   public void test() {
-    assert(true);
+    assertTrue(true);
     // TODO: Add your test logic here
     // You can generate random inputs and assert the expected output
     // For example:
